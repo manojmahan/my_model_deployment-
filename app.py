046934e76de073e45b13b179eda765e865ad8586
manojmahan <90147485+manojmahan@users.noreply.github.com>
2021-12-09 05:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def hello():
-    if request.method=="POST":
-        abcd = model.top20()
+    abcd = model.top20()
     return render_template("index.html",mk=abcd.to_html())
 if __name__ == '__main__':
     app.run(debug=True)
